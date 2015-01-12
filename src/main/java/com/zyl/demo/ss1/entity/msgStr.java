a2fed4ab0666846408f45d8e50c39c4bb5bca9d4
Z10 <olddoor@qq.com>
2015-01-12 22:34:19 +0800

    UI工具类

---
+++ src/main/java/com/zyl/demo/ss1/entity/msgStr.java
@@ -0,0 +1,54 @@
+package com.zyl.demo.ss1.entity;
+
+/**
+ * 
+ * JSON模型
+ * 
+ * 用户后台向前台返回的JSON对象
+ * 
+ * @author 孙宇
+ * 
+ */
+public class msgStr implements java.io.Serializable {
+
+	private boolean success = false;//状态
+
+	private String msg = ""; //提示信息
+
+	private Object obj = null;
+	
+	private String msgDetail;
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Object getObj() {
+		return obj;
+	}
+
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+
+	public String getMsgDetail() {
+		return msgDetail;
+	}
+
+	public void setMsgDetail(String msgDetail) {
+		this.msgDetail = msgDetail;
+	}
+
+}
